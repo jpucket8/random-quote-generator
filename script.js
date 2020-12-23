283fc9eb88a29b823af4cd757fb27abcce773fa0
JP <jpucket8@gmail.com>
2020-12-23 16:56:38 -0600

    Add twitter functionality

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const $authorText = document.getElementById("author");
 const $twitterBtn = document.getElementById("twitter");
 const $newQuoteBtn = document.getElementById("new-quote");
 
+// Event Listeners
+$newQuoteBtn.addEventListener("click", getQuote);
+$twitterBtn.addEventListener("click", tweetQuote);
+
 // Show New Quote
 function newQuote() {
   // Pick a random quote from 'quotes' array
@@ -23,9 +27,23 @@ async function getQuote() {
     const randomQuote = newQuote();
     $authorText.innerText = randomQuote.author;
     $quoteText.innerText = randomQuote.text;
+
+    // Reduce font size for long quotes
+    if (randomQuote.text.length > 50) {
+      $quoteText.classList.add("long-quote");
+    } else {
+      $quoteText.classList.remove("long-quote");
+    }
   } catch (error) {
     console.log(error);
   }
+}
+
+function tweetQuote() {
+  const quote = $quoteText.innerText;
+  const author = $authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, "_blank");
 }
 
 // On Load
